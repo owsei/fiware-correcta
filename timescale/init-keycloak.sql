7d06abb5d3b30439d231781081d3bec3991b2bd5
Pedro <pmesparza@itracasa.es>
2026-05-07 10:24:38 +0200

    docker compose de añadir seguridad

--- timescale/init-keycloak.sql
+++
@@ -1,8 +0,0 @@
-
--- create schema if not exists keycloak;
-
-
--- crea la base de datos y el esquema keycloak, y se conecta a la DB para crear el schema
-CREATE DATABASE keycloak OWNER postgres;
-\connect keycloak
-CREATE SCHEMA IF NOT EXISTS keycloak AUTHORIZATION postgres;
